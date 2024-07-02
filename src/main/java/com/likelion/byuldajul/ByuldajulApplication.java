a3e62d20f3839b7f592265224cd43bd5a6233e52
DDonghyeo <gsu9045000@gmail.com>
2024-07-02 18:08:05 +0900

    initial commit

---
+++ src/main/java/com/likelion/byuldajul/ByuldajulApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.byuldajul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByuldajulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByuldajulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/byuldajul/ByuldajulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.byuldajul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByuldajulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
