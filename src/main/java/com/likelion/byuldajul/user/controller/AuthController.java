3562067b7100c20f1d8b6eb0cd365b20b71b79c4
DDonghyeo <gsu9045000@gmail.com>
2024-07-10 21:54:05 +0900

    ♻️refactor : CORS 오류 설정 추가

--- src/main/java/com/likelion/byuldajul/user/controller/AuthController.java
+++ src/main/java/com/likelion/byuldajul/user/controller/AuthController.java
@@ -7,15 +7,13 @@ import com.likelion.byuldajul.user.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@CrossOrigin(originPatterns = "*")
 public class AuthController {
 
     private final AuthService authService;
