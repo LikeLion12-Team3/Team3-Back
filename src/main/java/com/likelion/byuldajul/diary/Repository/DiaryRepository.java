428c328c0e83133dd4e76f742ecc378df0fece4a
DDonghyeo <gsu9045000@gmail.com>
2024-07-12 12:53:03 +0900

    ♻️refactor : Diary Summary Update 날짜 쿼리 추가

--- src/main/java/com/likelion/byuldajul/diary/Repository/DiaryRepository.java
+++ src/main/java/com/likelion/byuldajul/diary/Repository/DiaryRepository.java
@@ -9,11 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    List<Diary> findAllByUser_Email(String email);
+
+    @Query("SELECT d FROM Diary d WHERE d.user.email = :email AND function('DATE', d.createdAt) = function('DATE', :date)")
+    List<Diary> findAllByUser_EmailAndCreatedAtDate(String email, LocalDate date);
 
     @Query("SELECT DISTINCT d FROM Diary d JOIN d.hashTags h JOIN d.user u " +
             "WHERE u.email = :email AND LOWER(h.name) LIKE LOWER(CONCAT('%', :query, '%'))")
